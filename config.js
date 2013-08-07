af07a3ca2ab43cbc0e25c5e9d47819383fda0f5f
Benjamin Staker <bassatcollege@gmail.com>
2013-08-07 09:14:28 +0100

    Added comments to config file

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 module.exports = function(cluster){
 
 	var config = {
+		/**
+		* This is where you define the services you'd like to run.
+		* @property	{String}	(required) Path to the service
+		* @property	{Int}		(optional) Port number to run off
+		*/
 		services: [
 			{
 				path: './log/'
